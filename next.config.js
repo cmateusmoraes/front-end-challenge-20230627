b673c5f549c1e7f1c757d07ba9ffd624f73a915a
Mateus Moraes <cmateusmoraes@gmail.com>
2023-07-01 23:02:56 -0300

    fix: fix CSP policy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 /** @type {import('next').NextConfig} */
+
+// Pagespeed optimization
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self'",
+  },
+];
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 module.exports = nextConfig;
